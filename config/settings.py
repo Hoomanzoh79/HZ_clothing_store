aac76af806279d3da49407cd9deefe5bd3793991
Hooman <hoomanemi2013@gmail.com>
2023-03-19 18:31:53 +0330

    add CustomUser

--- config/settings.py
+++ config/settings.py
@@ -31,12 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # django apps
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +131,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# CustomUser settings
+AUTH_USER_MODEL = 'accounts.CustomUser'
