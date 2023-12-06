2108c79a6032b536a4a106212d237d31ed3ec2cc
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-06 21:07:09 +0330

    add size to AddToCartForm

--- cart/forms.py
+++ cart/forms.py
@@ -1,7 +1,16 @@
 from django import forms
-
+from cloths.models import Cloth
 
 class AddToCartForm(forms.Form):
+    SIZE_CHOICES = (
+                    ('S', 'S'),
+                    ('M', 'M'),
+                    ('L','L'),
+                    ('XL', 'XL'),
+                    ('XXL', 'XXL'))
+    
     QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
+
     quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
     inplace = forms.BooleanField(required=False, widget=forms.HiddenInput)
+    size = forms.TypedChoiceField(choices=SIZE_CHOICES)
