c247aff96e6c828cf3d641d096ebb9a6f2033332
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2023-12-06 21:07:21 +0330

    add size to views

--- cart/views.py
+++ cart/views.py
@@ -4,12 +4,15 @@ from django.shortcuts import get_object_or_404,redirect
 from cloths.models import Cloth
 from .forms import AddToCartForm
 from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 
 def cart_detail_view(request):
     cart = Cart(request)
     for item in cart:
         item['update_quantity_form'] = AddToCartForm(initial={
+            'size':item['size'],
             'quantity': item['quantity'],
             'inplace': True,
         })
@@ -21,13 +24,17 @@ def add_to_cart_view(request, cloth_id):
     cart = Cart(request)
     cloth = get_object_or_404(Cloth, id=cloth_id)
     form = AddToCartForm(request.POST)
+    
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
-        cart.add(cloth, quantity, replace_current_quantity=cleaned_data['inplace'])
-
-    return redirect('cart:cart_detail')
-
+        size = cleaned_data['size']
+        if size in cloth.available_sizes():
+            cart.add(cloth, size ,quantity, replace_current_quantity=cleaned_data['inplace'])
+            return redirect('cart:cart_detail')
+        else:
+           messages.warning(request, _('Sorry! The chosen size is not available right now'))
+    return redirect('cloth_detail',pk=cloth_id)
 
 def remove_from_cart_view(request,cloth_id):
     cart = Cart(request)
