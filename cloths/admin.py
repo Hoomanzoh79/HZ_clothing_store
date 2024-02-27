caba99bbf5e2763f6ced80baefa0e7f8298fb62c
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-02-27 10:47:29 +0330

    fix some issues

--- cloths/admin.py
+++ cloths/admin.py
@@ -5,9 +5,9 @@ from jalali_date.admin import ModelAdminJalaliMixin
 
 @admin.register(Cloth)
 class ClothAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ['title', 'price', 'active', 'season', 'gender','sales' ]
+    list_display = ['title', 'price', 'active', 'season', 'gender', 'sales',  ]
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
-    list_display = ['cloth', 'author', 'body', 'active', ]
+    list_display = ['cloth', 'author', 'body', 'active',]
