03a99c5c5a109105f41f7cf33dffe38abac27bc8
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-06 12:36:54 +0330

    delete HomePageView

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,4 @@
 from django.views.generic import TemplateView
 
-
-class HomePageView(TemplateView):
-    template_name = 'pages/home.html'
-
-
 class AboutUsPageView(TemplateView):
     template_name = 'pages/about_us.html'
