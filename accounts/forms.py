268e86e5b70ceb388f36213d1f27df78574e118d
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-15 13:59:04 +0330

    add ProfileChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
+from django import forms
+from .models import Profile
 
 
 class CustomUserCreationForm(UserCreationForm):
@@ -12,3 +14,12 @@ class CustomUserChangeForm(UserChangeForm):
     class Meta:
         model = get_user_model()
         fields = ('username', 'email',)
+
+
+class ProfileChangeForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ("first_name","last_name",
+                  "address","phone_number",
+                  "email",
+                  )
