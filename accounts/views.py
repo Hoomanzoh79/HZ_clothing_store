2626677e0975835c20bceb9e82b50f9422519f3c
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-15 13:59:37 +0330

    add ProfileUpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Profile
+from .forms import ProfileChangeForm
+from django.urls import reverse
 
-# Create your views here.
+
+class ProfileUpdateView(generic.UpdateView):
+    model = Profile
+    form_class = ProfileChangeForm
+    template_name = "accounts/profile_update.html"
+
+    def get_success_url(self):
+        return reverse("index")
