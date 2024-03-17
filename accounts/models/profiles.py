f4c95d34a60d4d23357544cb199c775e481a85a9
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-17 18:37:34 +0330

    add verbose_name to fields

--- accounts/models/profiles.py
+++ accounts/models/profiles.py
@@ -1,16 +1,18 @@
 from django.db import models
-from .users import CustomUser
 from django.core.validators import RegexValidator
+from django.utils.translation import gettext as _
+
+from .users import CustomUser
 
 
 class Profile(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='profile')
-    first_name = models.CharField(max_length=50,blank=True)
-    last_name = models.CharField(max_length=50,blank=True)
-    address = models.TextField(blank=True)
+    first_name = models.CharField(max_length=50,blank=True,verbose_name=_('First name'))
+    last_name = models.CharField(max_length=50,blank=True,verbose_name=_('Last name'))
+    address = models.TextField(blank=True,verbose_name=_('Address'))
     phone_number = models.CharField(max_length=12, 
                                     validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],
-                                    blank=True,)
+                                    blank=True,verbose_name=_('Phone number'))
 
     def __str__(self):
         return f'profile : {self.user.username}'
