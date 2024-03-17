c2e6b4b33d9915fbb1ed6713cda3e1d35c0f4206
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-17 13:03:24 +0330

    change ProfileUpdateView path

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("<int:pk>/update/",views.ProfileUpdateView.as_view(),name="profile_update"),
+    path("update/",views.ProfileUpdateView.as_view(), name="profile_update"),
 ]
