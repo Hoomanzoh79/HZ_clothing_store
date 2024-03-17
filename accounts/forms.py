7f88ae03aefa7d42278b15b22e26dbf0b6423af5
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-17 18:22:00 +0330

    add widgets to ProfileChangeForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
 from django import forms
-from .models import Profile
+from django.utils.translation import gettext as _
 
+from .models import Profile
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
@@ -19,6 +20,10 @@ class CustomUserChangeForm(UserChangeForm):
 class ProfileChangeForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ("first_name","last_name",
-                  "address","phone_number",
+        fields = ("first_name", "last_name",
+                  "address", "phone_number",
                   )
+        widgets = {
+            'phone_number': forms.Textarea(attrs={'rows': 0, 'placeholder': _('Example : 09121111111 ')}),
+            'address': forms.Textarea(attrs={'rows': 3}),
+        }
