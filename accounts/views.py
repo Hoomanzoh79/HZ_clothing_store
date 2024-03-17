c59ef1bbe82fc4f8a08ced6faf56d0d3cda8bd58
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-17 13:03:09 +0330

    change get_object method for ProfileUpdateView

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views import generic
 from .models import Profile
 from .forms import ProfileChangeForm
 from django.urls import reverse
+from django.contrib import messages
+from django.utils.translation import gettext as _
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 
-class ProfileUpdateView(generic.UpdateView):
-    model = Profile
+class ProfileUpdateView(LoginRequiredMixin, generic.UpdateView):
     form_class = ProfileChangeForm
     template_name = "accounts/profile_update.html"
 
     def get_success_url(self):
-        return reverse("index")
+        messages.success(self.request, _('Your profile has been updated successfully'))
+        return reverse("profile_update")
+
+    def get_object(self):
+        user = self.request.user
+        try:
+            profile = Profile.objects.get(user_id=user.id)
+        except Profile.DoesNotExist:
+            profile = Profile.objects.create(user=user)
+        return profile
