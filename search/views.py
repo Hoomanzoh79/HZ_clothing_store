30ed52b15fbafa2a4acf7d048d00f3cb8f83d204
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-19 13:58:50 +0330

    delete some lines

--- search/views.py
+++ search/views.py
@@ -1,6 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.shortcuts import render
 from django.views.generic import ListView
 from cloths.models import Cloth
 
