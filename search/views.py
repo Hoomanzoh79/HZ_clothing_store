f66e1cf138043a1ecce05bff4662b020fdf09f2d
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-19 17:52:43 +0330

    add new context to SearchResultsView

--- search/views.py
+++ search/views.py
@@ -1,16 +1,27 @@
 from django.views.generic import ListView
 from cloths.models import Cloth
 
+
 class SearchResultsView(ListView):
     model = Cloth
     template_name = "search/search_results.html"
     context_object_name = 'cloths'
+    paginate_by = 10
 
     def get_queryset(self):  # new
-        query = self.request.GET.get("q")
+        search_query = self.request.GET.get("q")
+        if not search_query:
+            search_query = ""
         object_list = self.model.objects.filter(
-            title__icontains=query
+            title__icontains=search_query
         ) | self.model.objects.filter(
-            description__icontains=query
-        ) 
+            description__icontains=search_query
+        )
         return object_list
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchResultsView, self).get_context_data(**kwargs)
+        context.update({
+            'search_query': self.request.GET.get("q"),
+        })
+        return context
