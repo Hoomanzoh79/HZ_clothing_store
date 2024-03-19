7189b472d652ba75b4ad904f52a5ed263f5656f0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-19 13:20:53 +0330

    add SearchResultsView

--- search/views.py
+++ search/views.py
@@ -1,3 +1,19 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
+from django.views.generic import ListView
+from cloths.models import Cloth
 
-# Create your views here.
+class SearchResultsView(ListView):
+    model = Cloth
+    template_name = "search/search_results.html"
+    context_object_name = 'cloths'
+
+    def get_queryset(self):  # new
+        query = self.request.GET.get("q")
+        object_list = self.model.objects.filter(
+            title__icontains=query
+        ) | self.model.objects.filter(
+            description__icontains=query
+        ) 
+        return object_list
