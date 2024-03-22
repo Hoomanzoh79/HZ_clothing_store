73de3f69715d66f44e4027eaebd1efccf8b7166b
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-22 22:59:59 +0330

    add new urls from allauth

--- config/urls.py
+++ config/urls.py
@@ -14,21 +14,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include,re_path
 from django.conf.urls.static import static
 from django.conf import settings
-from allauth.account.views import (signup,login,logout,email,password_change,password_reset,password_reset_done)
-
+from allauth.account.views import (signup, login, logout, email, password_change, password_reset,password_reset_done, email_verification_sent,confirm_email,password_reset_from_key,password_reset_from_key_done)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),
-    path('profile/',include('accounts.urls')),
+    path('profile/', include('accounts.urls')),
     path('', include('cloths.urls')),
     path('cart/', include('cart.urls')),
     path('order/', include('orders.urls')),
     path('payment', include('payment.urls')),
-    path('search/',include('search.urls')),
+    path('search/', include('search.urls')),
     # All Auth
     path("account/login/", login, name="account_login"),
     path("account/logout/", logout, name="account_logout"),
@@ -36,8 +35,18 @@ urlpatterns = [
     path("account/email/", email, name="account_email"),
     path("account/password/reset/", password_reset, name="account_reset_password"),
     path("account/password/reset/done/", password_reset_done, name="account_reset_password_done"),
+    re_path(
+        r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        password_reset_from_key,
+        name="account_reset_password_from_key",
+    ),
+    path(
+        "password/reset/key/done/",
+        password_reset_from_key_done,
+        name="account_reset_password_from_key_done",
+    ),
     path("account/password/change/", password_change, name="account_change_password"),
+    path("account/confirm-email/",email_verification_sent,name="account_email_verification_sent"),
+    re_path(r"^confirm-email/(?P<key>[-:\w]+)/$",confirm_email,name="account_confirm_email"),
     # Rosetta (i18n)
-    path('rosetta/', include('rosetta.urls')),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('rosetta/', include('rosetta.urls')),] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
