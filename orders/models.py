de13d149cfc362ec0f793a0a13f2d1b4963861b0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-24 11:53:16 +0330

    add verbose name to Order fields

--- orders/models.py
+++ orders/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 from django.conf import settings
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext as _
 
 
 class Order(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     is_paid = models.BooleanField(default=False)
 
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=15)
-    address = models.CharField(max_length=700)
-    order_notes = models.CharField(max_length=700, blank=True)
+    first_name = models.CharField(max_length=50,verbose_name=_('First name'))
+    last_name = models.CharField(max_length=50,verbose_name=_('Last name'))
+    address = models.TextField(verbose_name=_('Address'))
+    phone_number = models.CharField(max_length=12,
+                                    validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],
+                                    blank=True,verbose_name=_('Phone number'))
+
+    order_notes = models.CharField(max_length=700, blank=True,verbose_name=_('Order notes'))
 
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
