1efe7c92f4e485318f0e81a9fe9af50bfb60ba76
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-25 12:39:23 +0330

    add verify url

--- payment/urls.py
+++ payment/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'payment'
 
 urlpatterns = [
-    path('process/', views.payment_process, name='payment_process'),
+    path('process/', views.PaymentProcessView.as_view(), name='payment_process'),
+    path('verify/', views.PaymentVerify.as_view(), name='payment_verify'),
 ]
