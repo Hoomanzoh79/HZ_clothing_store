a0b5cb8137a58ec17e3e25d68a42ebe9e4169c3a
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-01 11:44:27 +0330

    temporary change debug to false to test 404 template

--- config/settings.py
+++ config/settings.py
@@ -28,6 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = False # to test 404 template
 DEBUG = env("DJANGO_DEBUG")
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
