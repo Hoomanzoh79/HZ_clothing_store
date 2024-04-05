47e1627c0e2c30271811b2d776af2445dae91f23
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-05 16:20:03 +0330

    customize admin settings

--- cloths/admin.py
+++ cloths/admin.py
@@ -6,8 +6,15 @@ from jalali_date.admin import ModelAdminJalaliMixin
 @admin.register(Cloth)
 class ClothAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
     list_display = ['title', 'price', 'active', 'season', 'gender', 'sales',  ]
+    list_per_page = 10
+    list_editable = ['price','active']
+    ordering = ['-datetime_created']
+
 
 
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
     list_display = ['cloth', 'author', 'body', 'active',]
+    list_per_page = 10
+    list_editable = ['active']
+    ordering = ['-datetime_created']
