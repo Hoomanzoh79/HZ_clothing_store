4436fd48e9710896a5c62da51f7bf52ce024a26c
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-06 11:34:54 +0330

    customize admin for user and profile

--- accounts/admin.py
+++ accounts/admin.py
@@ -9,11 +9,16 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     form = CustomUserChangeForm
     add_form = CustomUserCreationForm
-    list_display = ('email', 'username',
+    list_display = ('id','email', 'username',
                     'is_staff','is_superuser',
                     )
+    ordering = ['-id']
+    list_per_page = 10
+
 
 @admin.register(Profile)
 class ProfileAdmin(admin.ModelAdmin):
     model = Profile
-    fields = ['user','first_name','last_name','address','phone_number',]
+    list_display = ('id','user','first_name','last_name','phone_number',)
+    ordering = ['-id']
+    list_per_page = 10
