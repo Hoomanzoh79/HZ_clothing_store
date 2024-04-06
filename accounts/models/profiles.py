ae1ebe5b607ff0309edc2941153ffdcaa12d98bd
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-06 13:18:52 +0330

    add datetime for profile

--- accounts/models/profiles.py
+++ accounts/models/profiles.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.core.validators import RegexValidator
 from django.utils.translation import gettext as _
+from django.utils import timezone
 
 from .users import CustomUser
 
@@ -13,6 +14,9 @@ class Profile(models.Model):
     phone_number = models.CharField(max_length=12, 
                                     validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],
                                     blank=True,verbose_name=_('Phone number'))
+    
+    datetime_created = models.DateTimeField(default=timezone.now)
+    datetime_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return f'profile : {self.user.username}'
