6773afaed9335979946115e036852cff5ba829d6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-07 16:17:29 +0330

    add about_us

--- pages/urls.py
+++ pages/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('aboutus/', views.AboutUsPageView.as_view(), name='about_us'),
+    path('about_us/', views.AboutUsPageView.as_view(), name='about_us'),
     path("", views.IndexView.as_view(), name="index"),
 ]
