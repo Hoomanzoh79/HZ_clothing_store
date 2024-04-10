e37b63bff13d37f1f59f8dc56b6ebceeeb6d4c66
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-11 01:00:39 +0330

    change url for ClothDetailView and CommentCreateView

--- cloths/urls.py
+++ cloths/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('list/female/',views.FemaleView.as_view(),name='female_list'),
     path('list/male/',views.MaleView.as_view(),name='male_list'),
     path('list/highest-selling/',views.HighestSellingView.as_view(),name='highest_selling'),
-    path(r'list/(?P<slug>[0-9]+)/$', views.ClothDetailView.as_view(), name='cloth_detail'),
-    path('list/comment/<int:cloth_id>/', views.CommentCreateView.as_view(), name='comment_create'),
+    path(r'list/<slug>[0-9]+/', views.ClothDetailView.as_view(), name='cloth_detail'),
+    path(r'list/comment/<slug>[0-9]+/', views.CommentCreateView.as_view(), name='comment_create'),
 ]
