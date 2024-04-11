9a2623114dfe1b5adaf1679a413aa1630eb2441b
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-11 12:27:04 +0330

    add brand to cloth model

--- cloths/admin.py
+++ cloths/admin.py
@@ -9,12 +9,12 @@ from .models import Cloth, Comment
 
 @admin.register(Cloth)
 class ClothAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ['id','title','slug','price','category','season', 'gender', 'sales','inventory',"inventory_status","comments_count"]
+    list_display = ['id','title','slug','brand','price','category','season', 'gender', 'sales','inventory',"inventory_status","comments_count"]
     list_per_page = 10
     list_editable = ['price','inventory','category']
     ordering = ['-datetime_created']
     list_filter = ['datetime_created','gender','season']
-    search_fields = ['title__istartswith']
+    search_fields = ['title__istartswith','brand__istartswith']
     list_display_links = ['id','title']
     prepopulated_fields = {
         'slug' : ['title',]
