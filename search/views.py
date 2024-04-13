61aa6ccc3bc74fbe4cf6fd690a0321c275c02ecf
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-13 11:43:24 +0330

    change FilterResultsView

--- search/views.py
+++ search/views.py
@@ -1,5 +1,12 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.views.generic import ListView
+from django.shortcuts import get_object_or_404
+from django.views.generic.edit import FormMixin
+from django.shortcuts import render
+
 from cloths.models import Cloth
+from .forms import ClothFilterForm
 
 
 class SearchResultsView(ListView):
@@ -49,24 +56,19 @@ class CategoryFilterResultsView(ListView):
         return context
 
 
-class FilterResultsView(ListView):
+class FilterResultsView(FormMixin,ListView):
     model = Cloth
     template_name = "search/filter_results.html"
-    context_object_name = 'cloths'
 
-    def get_queryset(self):  # new
-        filter_query = self.request.GET.post("q")
-        if not filter_query:
-            filter_query = ""
-        object_list = self.model.objects.filter(
-            category=filter_query
-        )
-        print(filter_query)
-        return object_list
+    def get(self, request, *args, **kwargs):
+        form_class = ClothFilterForm
+        self.form = self.get_form(form_class)
+        self.category = self.request.GET.get("category")
+        self.season = self.request.GET.get("season")
+        self.gender = self.request.GET.get("gender")
+        print(self.category)
+        cloths = self.model.objects.filter(category=self.category)
+        return render(request,template_name=self.template_name,context={'cloths':cloths})
 
-    def get_context_data(self, **kwargs):
-        context = super(FilterResultsView, self).get_context_data(**kwargs)
-        context.update({
-            'filter_query': self.request.GET.get("q"),
-        })
-        return context
+    def post(self, request, *args, **kwargs):
+        return self.get(request, *args, **kwargs)
