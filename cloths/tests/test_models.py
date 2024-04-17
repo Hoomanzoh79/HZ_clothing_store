b7365bcf175d40e5ac48b6504a4a16b02e90dbc8
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-17 22:13:55 +0330

    add first tests

--- cloths/tests/test_models.py
+++ cloths/tests/test_models.py
@@ -1,18 +1,16 @@
 from django.test import TestCase
 from django.template.defaultfilters import slugify
 
-
 from cloths.models import Cloth,Comment
+from accounts.models import CustomUser
 
 
-class TestCommentModel(TestCase):
-    def setUp(self):
-        self.valid_data = {
+CLOTH_VALID_DATA =  {
             "title" : "test cloth",
+            "slug" : slugify("test cloth"),
             "description" : "test description",
-            "slug" : slugify("title"),
             "price" : 1500000,
-            "sizes": (('S', 'S')),
+            "sizes": "S",
             "gender": "male",
             "sales":0,
             "inventory":20,
@@ -20,8 +18,28 @@ class TestCommentModel(TestCase):
             "brand":"adidas",
         }
 
-    def test_create_cloth_valid_data(self):
-        cloth = Cloth.objects.create(self.valid_data)
+class TestClothModel(TestCase):
+    def test_create_cloth(self):
+        cloth = Cloth.objects.create(**CLOTH_VALID_DATA)
         self.assertTrue(Cloth.objects.filter(pk=cloth.id).exists())
-        self.assertEquals(cloth.title,'test cloth')
-        self.assertEquals(cloth.objects.count(),1)
+        self.assertTrue(Cloth.objects.filter(slug=cloth.slug).exists())
+        self.assertEquals(Cloth.objects.count(),1)
+        self.assertEquals(cloth.title,"test cloth")
+        self.assertEquals(cloth.slug,slugify(cloth.title))
+    
+class TestCommentModel(TestCase):
+    def setUp(self):
+        self.cloth = Cloth.objects.create(**CLOTH_VALID_DATA)
+        self.author = CustomUser.objects.create(email="test email",password="*7S^dasadDSA1")
+        self.valid_data = {
+            "author":self.author,
+            "cloth":self.cloth,
+            "body":"test comment",
+            "active":True,
+        }
+    
+    def test_create_comment(self):
+        comment = Comment.objects.create(**self.valid_data)
+        self.assertEquals(Comment.objects.count(),1)
+        self.assertTrue(Comment.objects.filter(pk=comment.id).exists())
+        self.assertEquals(comment.body,"test comment")
