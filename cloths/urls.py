de85e88017ccbc92a3a975de5884b84cb7ad12f0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-18 17:39:41 +0330

    change the order of urls

--- cloths/urls.py
+++ cloths/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('list/', views.ClothsListView.as_view(), name='cloths_list'),
+    path(r'list/<str:slug>[0-9]+/', views.ClothDetailView.as_view(), name='cloth_detail'),
+    path(r'list/comment/<str:slug>[0-9]+/', views.CommentCreateView.as_view(), name='comment_create'),
     path('list/female/',views.FemaleView.as_view(),name='female_list'),
     path('list/male/',views.MaleView.as_view(),name='male_list'),
     path('list/highest-selling/',views.HighestSellingView.as_view(),name='highest_selling'),
-    path(r'list/<slug>[0-9]+/', views.ClothDetailView.as_view(), name='cloth_detail'),
-    path(r'list/comment/<slug>[0-9]+/', views.CommentCreateView.as_view(), name='comment_create'),
 ]
