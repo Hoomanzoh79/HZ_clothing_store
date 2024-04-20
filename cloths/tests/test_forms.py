602fc762364345c95597fee3c6162850aa9a4503
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-20 10:23:15 +0330

    add 2 tests for comment form

---
+++ cloths/tests/test_forms.py
@@ -0,0 +1,15 @@
+from django.test import TestCase
+
+from accounts.models import CustomUser
+from cloths.forms import CommentForm
+
+class TestCommentForm(TestCase):
+    def test_comment_form_valid_data(self):
+        comment_form = CommentForm(data={
+            'body' : 'test valid comment form',
+        })
+        self.assertTrue(comment_form.is_valid())
+    
+    def test_comment_form_invalid_data(self):
+        comment_form = CommentForm(data={})
+        self.assertFalse(comment_form.is_valid())
