5315bf3ceb288f851bd71104805703765b7b1868
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-23 18:09:20 +0330

    change some names

--- cart/tests/test_forms.py
+++ cart/tests/test_forms.py
@@ -3,14 +3,14 @@ from django.test import TestCase
 from cart.forms import AddToCartForm
 
 
-class TestCommentForm(TestCase):
+class TestCartForm(TestCase):
     def test_add_to_cart_form_valid_data(self):
-        comment_form = AddToCartForm(data={
+        add_to_cart_form = AddToCartForm(data={
             'quantity' : 2,
             'size': "L",
         })
-        self.assertTrue(comment_form.is_valid())
+        self.assertTrue(add_to_cart_form.is_valid())
     
     def test_add_to_cart_form_invalid_data(self):
-        comment_form = AddToCartForm(data={})
-        self.assertFalse(comment_form.is_valid())
+        add_to_cart_form = AddToCartForm(data={})
+        self.assertFalse(add_to_cart_form.is_valid())
