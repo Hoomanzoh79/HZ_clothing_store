0bcfc24967307d42dd01edf38adf290fef9a74a6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-23 18:25:41 +0330

    add test for invalid phone_number

--- orders/tests/test_forms.py
+++ orders/tests/test_forms.py
@@ -9,12 +9,13 @@ from accounts.models import CustomUser
 class TestOrderForm(TestCase):
     def test_order_form_valid_data(self):
         order_form = OrderForm(data={
-            'first_name' : "test firstname",
-            'last_name': "test lastname",
-            'phone_number':"09361958721",
-            'address':"test address",
+            "first_name" : "test firstname",
+            "last_name": "test lastname",
+            "phone_number" :"09361958721",
+            "address":"test address",
         })
         self.assertTrue(order_form.is_valid())
+        self.assertIn("first_name", order_form.fields)
     
     def test_order_form_invalid_phone_number(self):
         order_form = OrderForm(data={
@@ -24,6 +25,7 @@ class TestOrderForm(TestCase):
             'address':"test address",
         })
         self.assertFalse(order_form.is_valid())
+        self.assertIn('phone_number', order_form.errors.keys())
     
     def test_order_form_invalid_data(self):
         order_form = OrderForm(data={})
