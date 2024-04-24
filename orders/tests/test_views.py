8ea9f7f609348a7a1beb70414b552ada1c3123eb
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-24 17:11:50 +0330

    add final tests for order

--- orders/tests/test_views.py
+++ orders/tests/test_views.py
@@ -21,28 +21,30 @@ CLOTH_VALID_DATA =  {
 
 class TestOrderView(TestCase):
     def setUp(self):
-        self.user = CustomUser.objects.create(email="testemail@email.com",password="*7S^dasadDSA1")
+        self.user = CustomUser.objects.create(email="testorder@gmail.com",password="*7S^dasadDSA1")
         self.cloth = Cloth.objects.create(**CLOTH_VALID_DATA)
     
     def test_order_create_view_unauthorized(self):
+        """redirects the user to login page,POST request or GET request doesn't matter"""
         url = reverse('orders:order_create')
         response = self.client.post(url)
         self.assertEquals(response.status_code,302)
         self.assertRedirects(response,reverse("account_login"))
 
     def test_order_create_view_authorized_empty_cart(self):
+        """redirects the user to cart_detail because cart is empty,here user can only use GET request
+        (we won't give away the form so they can't use POST request)"""
         self.client.force_login(user=self.user)
         url = reverse('orders:order_create')
-        response = self.client.post(url)
+        response = self.client.get(url)
         self.assertEquals(response.status_code,302)
         self.assertRedirects(response,reverse("cart:cart_detail"))
     
-    # def test_order_create_view_authorized(self):
-    #     self.client.force_login(user=self.user)
-    #     cart = Cart(self.client)
-    #     cart.save()
-    #     cart.add(self.cloth,quantity=2,size="S")
-    #     url = reverse('orders:order_create')
-    #     response = self.client.post(url)
-    #     self.assertEquals(response.status_code,302)
-    #     self.assertRedirects(response,reverse("payment:payment_process"))
+    def test_order_create_view_authorized(self):
+        """cart is not empty and user is authenticated"""
+        self.client.force_login(user=self.user)
+        cart = Cart(self.client)
+        cart.add(self.cloth,quantity=2,size="S")
+        url = reverse('orders:order_create')
+        response = self.client.post(url)
+        self.assertEquals(response.status_code,200)
