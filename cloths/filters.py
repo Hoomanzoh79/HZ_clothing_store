85a7bb89c3d083a65457b89f79c84e38b46feb25
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-26 00:31:03 +0330

    add ClothFilter

--- cloths/filters.py
+++ cloths/filters.py
@@ -1,10 +1,24 @@
 import django_filters
+from django import forms
+
+
 from .models import Cloth
 
 class ClothFilter(django_filters.FilterSet):
-
-    brand = django_filters.CharFilter(lookup_expr='icontains')
+    price = django_filters.RangeFilter()
+    category = django_filters.MultipleChoiceFilter(
+        choices=Cloth.CATEGORIES,
+        widget=forms.CheckboxSelectMultiple()
+    )
+    gender = django_filters.MultipleChoiceFilter(
+        choices=Cloth.GENDER_CHOICES,
+        widget=forms.CheckboxSelectMultiple()
+    )
+    season = django_filters.MultipleChoiceFilter(
+        choices=Cloth.SEASON_CHOICES,
+        widget=forms.CheckboxSelectMultiple()
+    )
 
     class Meta:
         model = Cloth
-        fields = ['brand']
+        fields = ["brand"]
