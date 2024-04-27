0bd7cce098fa5fb3f145d6aeb96134be57d46865
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-27 10:15:14 +0330

    add new fields to ClothFilter

--- cloths/filters.py
+++ cloths/filters.py
@@ -1,11 +1,26 @@
 import django_filters
 from django import forms
-
+from django.forms.widgets import NumberInput
+from django.utils.translation import gettext as _
 
 from .models import Cloth
 
+EXAMPLE_BRANDS = [
+        ('nike',_('nike')),
+        ('adidas',_('adidas')),
+        ('jeanwest',_('jeanwest')),
+        ('body spinner',_('body spinner')),
+        ('LC MAN', _('lc man')),
+    ]
 class ClothFilter(django_filters.FilterSet):
-    price = django_filters.RangeFilter()
+    brand = django_filters.CharFilter(label=_('search brand'),
+                                      widget=forms.TextInput(attrs={'placeholder': _('type the brand name')})
+                                      )
+    popular_brands = django_filters.MultipleChoiceFilter(field_name='brand',
+        label=_('popular brands'),                                           
+        choices=EXAMPLE_BRANDS,
+        widget=forms.CheckboxSelectMultiple()
+    )
     category = django_filters.MultipleChoiceFilter(
         choices=Cloth.CATEGORIES,
         widget=forms.CheckboxSelectMultiple()
@@ -18,7 +33,11 @@ class ClothFilter(django_filters.FilterSet):
         choices=Cloth.SEASON_CHOICES,
         widget=forms.CheckboxSelectMultiple()
     )
+    price__gt = django_filters.NumberFilter(field_name='price', lookup_expr 
+    ='gte', label=_('min price'), widget=NumberInput(attrs={'placeholder': '0'}))
+    price__lt = django_filters.NumberFilter(field_name='price', lookup_expr 
+    = 'lte', label=_('max price'), widget=NumberInput(attrs={'placeholder': _('max price')}))
 
     class Meta:
         model = Cloth
-        fields = ["brand"]
+        fields = []
