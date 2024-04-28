6132147bc0f1d6b1b6bac2d5f3ad8fd25e6e2262
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-28 10:50:48 +0330

    change form to get available sizes

--- cart/forms.py
+++ cart/forms.py
@@ -1,16 +1,23 @@
 from django import forms
 from cloths.models import Cloth
 
-class AddToCartForm(forms.Form):
-    SIZE_CHOICES = (
-                    ('S', 'S'),
-                    ('M', 'M'),
-                    ('L','L'),
-                    ('XL', 'XL'),
-                    ('XXL', 'XXL'))
+class AddToCartForm(forms.ModelForm):
     
     QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 26)]
 
     quantity = forms.TypedChoiceField(choices=QUANTITY_CHOICES, coerce=int)
     inplace = forms.BooleanField(required=False, widget=forms.HiddenInput)
-    size = forms.TypedChoiceField(choices=SIZE_CHOICES)
+
+    class Meta:
+        model = Cloth
+        fields = ['sizes']
+    
+    
+    def __init__(self, pk, *args, **kwargs):
+            super(AddToCartForm, self).__init__(*args, **kwargs)
+            sizes = tuple(Cloth.objects.get(pk=pk).sizes)
+            sizes_list = []
+            for item in sizes:
+                sizes_list.append((item, item))
+            self.fields['sizes'] = forms.ChoiceField(choices=sizes_list)
+    
