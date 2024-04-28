70acb20bb285e2a58dc8c606c28263c826b7a444
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-28 10:51:00 +0330

    delete update quantity

--- cart/views.py
+++ cart/views.py
@@ -10,12 +10,7 @@ from django.utils.translation import gettext as _
 
 def cart_detail_view(request):
     cart = Cart(request)
-    for item in cart:
-        item['update_quantity_form'] = AddToCartForm(initial={
-            'size':item['size'],
-            'quantity': item['quantity'],
-            'inplace': True,
-        })
+
     return render(request, 'cart/cart_detail.html', {'cart': cart})
 
 
@@ -23,18 +18,17 @@ def cart_detail_view(request):
 def add_to_cart_view(request, cloth_id):
     cart = Cart(request)
     cloth = get_object_or_404(Cloth, id=cloth_id)
-    form = AddToCartForm(request.POST)
+    pk = cloth.pk
+    form = AddToCartForm(data=request.POST,instance=cloth,pk=pk)
     
     if form.is_valid():
         cleaned_data = form.cleaned_data
         quantity = cleaned_data['quantity']
-        size = cleaned_data['size']
-        if size in cloth.available_sizes():
-            cart.add(cloth, size ,quantity, replace_current_quantity=cleaned_data['inplace'])
-            messages.success(request, _('Product has been added successfully to the cart'))
-            return redirect('cart:cart_detail')
-        else:
-           messages.warning(request, _('Sorry! The chosen size is not available right now'))
+        size = cleaned_data['sizes']
+        cart.add(cloth, size ,quantity, replace_current_quantity=cleaned_data['inplace'])
+        messages.success(request, _('Product has been added successfully to the cart'))
+        return redirect('cart:cart_detail')
+
     return redirect(cloth.get_absolute_url())
 
 
