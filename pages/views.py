b7d267f2a8fa66190257e5240eb26acb9e199229
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-30 21:48:03 +0330

    delete useless lines

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 
-from cloths.models import Cloth, Comment
+from cloths.models import Cloth
 
 
 class AboutUsPageView(generic.TemplateView):
