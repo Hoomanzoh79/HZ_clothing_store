943e0b248771f8f6439b3d3024b12fabce43d914
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-30 13:40:05 +0330

    add color to tests

--- cart/tests/test_cart.py
+++ cart/tests/test_cart.py
@@ -1,7 +1,7 @@
 from django.test import TestCase,Client,RequestFactory
 from django.template.defaultfilters import slugify
 
-from cloths.models import Cloth
+from cloths.models import Cloth,Color
 from cart.cart import Cart
 
 
@@ -36,12 +36,14 @@ class TestCart(TestCase):
     def setUp(self):
         self.client = Client()
         self.cloth1 = Cloth.objects.create(**CLOTH_VALID_DATA1)
+        self.color1 = self.cloth1.colors.create(color_name="red")
         self.cloth2 = Cloth.objects.create(**CLOTH_VALID_DATA2)
+        self.color2 = self.cloth2.colors.create(color_name="blue")
         self.factory = RequestFactory()
     
     def test_add_cloth_to_cart(self):
         cart = Cart(self.client)
-        cart.add(self.cloth1,quantity=1,size="XL")
+        cart.add(self.cloth1,quantity=1,size="XL",color=self.color1)
         for item in cart:
             quantity = item["quantity"]
             size = item["size"]
@@ -51,19 +53,19 @@ class TestCart(TestCase):
     
     def test_add_multiple_cloths_to_cart(self):
         cart = Cart(self.client)
-        cart.add(self.cloth1,quantity=1,size="XL")
-        cart.add(self.cloth2, quantity=2,size="L")
+        cart.add(self.cloth1,quantity=1,size="XL",color=self.color1)
+        cart.add(self.cloth2, quantity=2,size="L",color=self.color2)
         self.assertEquals(len(cart), 3)
     
     def test_remove_cloth_from_cart(self):
         cart = Cart(self.client)
-        cart.add(self.cloth1,quantity=2,size="XL")
+        cart.add(self.cloth1,quantity=2,size="XL",color=self.color1)
         cart.remove(self.cloth1)
         self.assertEquals(len(cart), 0)
     
     def test_clear_cart(self):
         cart = Cart(self.client)
-        cart.add(self.cloth1,quantity=1,size="XL")
-        cart.add(self.cloth2, quantity=2,size="L")
+        cart.add(self.cloth1,quantity=1,size="XL",color=self.color1)
+        cart.add(self.cloth2, quantity=2,size="L",color=self.color2)
         cart.clear()
         self.assertEquals(len(cart),0)
