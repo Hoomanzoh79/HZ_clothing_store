1ee41f6a7e5108b8af173fa2636d95726597c3ff
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-01 12:04:21 +0330

    add postal code

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 from django.contrib.auth import get_user_model
 from django import forms
 from django.utils.translation import gettext as _
+from django.forms import widgets
 
 from .models import Profile
 
@@ -21,9 +22,10 @@ class ProfileChangeForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ("first_name", "last_name",
-                  "address", "phone_number",
+                  "address","postal_code","phone_number",
                   )
         widgets = {
-            'phone_number': forms.Textarea(attrs={'rows': 0, 'placeholder': _('Example : 09121111111 ')}),
+            'phone_number': forms.NumberInput(attrs={'rows': 0, 'placeholder': _('Example : 09121111111 ')}),
             'address': forms.Textarea(attrs={'rows': 3}),
+            'postal_code':forms.NumberInput(attrs={'rows': 0,})
         }
