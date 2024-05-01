2668617924baaecb40816d4441afd0269725a761
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-01 11:48:39 +0330

    Add field postal_code to profile

--- accounts/models/profiles.py
+++ accounts/models/profiles.py
@@ -11,6 +11,10 @@ class Profile(models.Model):
     first_name = models.CharField(max_length=50,blank=True,verbose_name=_('First name'))
     last_name = models.CharField(max_length=50,blank=True,verbose_name=_('Last name'))
     address = models.TextField(blank=True,verbose_name=_('Address'))
+    postal_code = models.CharField(max_length=10,
+                                validators=[RegexValidator(regex="^(?!(\d)\1{3})[13-9]{4}[1346-9][ -]?[013-9]{5}$|^$")],
+                                verbose_name=_("Postal code"),null=True
+                                )
     phone_number = models.CharField(max_length=12, 
                                     validators=[RegexValidator(regex="^[0][9][0-9][0-9]{8,8}$")],
                                     blank=True,verbose_name=_('Phone number'))
