f00213e881af22ab25c5fa9c50741fbad8c27174
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-01 20:23:16 +0330

    add new configs for liara

--- config/settings.py
+++ config/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("DJANGO_SECRET_KEY")
+SECRET_KEY = config("DJANGO_SECRET_KEY",default="test")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("DJANGO_DEBUG", cast=bool)
+DEBUG = config("DJANGO_DEBUG", cast=bool,default=False)
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
@@ -209,7 +209,7 @@ MEDIA_URL = '/media/'
 
 
 # payment and zarinpal
-SANDBOX = config("ZARINPAL_SANDBOX", cast=bool)
+SANDBOX = config("ZARINPAL_SANDBOX", cast=bool,default=True)
 MERCHANT = config("ZARINPAL_MERCHANT_ID",default="test marchant")
 
 # cart
