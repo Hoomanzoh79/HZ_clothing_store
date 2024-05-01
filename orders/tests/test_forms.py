5ee32e4a199c4749e354bc3459d01171dc61c3b7
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-01 13:22:37 +0330

    add new test

--- orders/tests/test_forms.py
+++ orders/tests/test_forms.py
@@ -9,6 +9,7 @@ class TestOrderForm(TestCase):
             "last_name": "test lastname",
             "phone_number" :"09361958721",
             "address":"test address",
+            "postal_code":"3149637441"
         })
         self.assertTrue(order_form.is_valid())
         self.assertIn("first_name", order_form.fields)
@@ -19,9 +20,21 @@ class TestOrderForm(TestCase):
             'last_name': "test lastname",
             'phone_number':"9361958721",
             'address':"test address",
+            'postal_code':'3149637441',
         })
         self.assertFalse(order_form.is_valid())
         self.assertIn('phone_number', order_form.errors.keys())
+    
+    def test_order_form_invalid_postal_code(self):
+        order_form = OrderForm(data={
+            'first_name' : "test firstname",
+            'last_name': "test lastname",
+            'phone_number':"09361958721",
+            'address':"test address",
+            'postal_code':'3149637441dsa',
+        })
+        self.assertFalse(order_form.is_valid())
+        self.assertIn('postal_code', order_form.errors.keys())
     
     def test_order_form_invalid_data(self):
         order_form = OrderForm(data={})
